e568f9ba49ecbab9285044d595ebb278a2b7cff4
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-08-20 21:59:09 +0600

    B9A3 Hockeys

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      'manrope': ["Manrope", "sans-serif"]
+    },
+    extend: {
+      colors:{
+        'coral-red': '#ff4340',
+        'heading': '#131318',
+        'heading-d': '#fff',
+        'paragraph': '#13131899',
+        'paragraph-d': '#ffffffa6',
+      }
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  darkMode: 'class',
+}
+
