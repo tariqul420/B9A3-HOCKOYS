24700a6d3b3b0053d5968d94660a09eaf4478f40
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-08-21 12:43:48 +0600

    Our New Products

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,10 @@ module.exports = {
     extend: {
       colors: {
         "coral-red": "#ff4340",
-        "heading": "#131318",
+        heading: "#131318",
         "heading-d": "#fff",
-        "paragraph": "#13131899",
+        paragraph: "#13131899",
         "paragraph-d": "#ffffffa6",
-      },
-      backgroundImage: {
-        'img-2': "url('/images/2.png')",
-        'img-3': "url('/images/3.png')",
-        'img-4': "url('/images/4.png')",
       },
     },
   },
