032ec9b8bdda6489798794ab02f2e635f763fa3e
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-08-21 10:59:06 +0600

    add background img

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,23 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     fontFamily: {
-      'manrope': ["Manrope", "sans-serif"]
+      manrope: ["Manrope", "sans-serif"],
     },
     extend: {
-      colors:{
-        'coral-red': '#ff4340',
-        'heading': '#131318',
-        'heading-d': '#fff',
-        'paragraph': '#13131899',
-        'paragraph-d': '#ffffffa6',
-      }
+      colors: {
+        "coral-red": "#ff4340",
+        "heading": "#131318",
+        "heading-d": "#fff",
+        "paragraph": "#13131899",
+        "paragraph-d": "#ffffffa6",
+      },
+      backgroundImage: {
+        'img-2': "url('/images/2.png')",
+        'img-3': "url('/images/3.png')",
+        'img-4': "url('/images/4.png')",
+      },
     },
   },
-  plugins: [
-    require('daisyui'),
-  ],
-  darkMode: 'class',
-}
-
+  plugins: [require("daisyui")],
+  darkMode: "class",
+};
